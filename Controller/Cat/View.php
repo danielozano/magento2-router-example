aa415f20d17614ade4abb380e3d3fe1520e3fc44
Daniel Lozano <dn.lozano.m@gmail.com>
2019-04-22 11:47:07 +0200

    add composer.json

--- Controller/Cat/View.php
+++ Controller/Cat/View.php
@@ -12,10 +12,7 @@ class View extends Action
     /**
      * Execute action based on request and return result
      *
-     * Note: Request will be added as operation argument in future
-     *
      * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
-     * @throws \Magento\Framework\Exception\NotFoundException
      */
     public function execute()
     {
